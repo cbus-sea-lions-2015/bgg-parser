6b54422ad775faeffe928fb312fffc7336e0e0eb
Peter Mueller <peter.kh.mueller@gmail.com>
2015-04-17 10:42:03 -0400

    initial commit

---
+++ bgg-parser.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'bgg-parser'
+  s.version     = '0.0.1'
+  s.date        = '2015-04-16'
+  s.summary     = "Parses BoardGameGeek"
+  s.description = "No fucks given"
+  s.authors     = ["Peter Mueller","Elizabeth Blackburn"]
+  s.email       = ''
+  s.files       = ["lib/bgg-parser.rb"]
+  s.homepage    = ''
+  s.license     = 'MIT'
+end
---
+++ lib/bgg-parser.rb
@@ -0,0 +1,31 @@
+class BggParser
+  def self.parse(raw_json, library)
+    raw_json["item"].each do |game_json|
+      game = Game.new
+      game.bgg_id        = game_json["id"]
+      game.name          = game_json["name"][0]["value"]
+      game.description   = game_json["description"]
+      game.minplayers    = game_json["minplayers"][0]["value"].to_i
+      game.maxplayers    = game_json["maxplayers"][0]["value"].to_i
+      game.minplaytime   = game_json["minplaytime"][0]["value"].to_i
+      game.maxplaytime   = game_json["maxplaytime"][0]["value"].to_i
+      game.poll          = game_json["poll"][0] if game_json["poll"][0]["name"] == "suggested_numplayers"
+      game.image_url     = game_json["image"][0]
+      game.thumbnail_url = game_json["thumbnail"][0]
+
+      game.save
+
+      GamesLibrary.create game: game, library: library
+
+      game_json["link"].each do |row|
+        if row["type"] == "boardgamecategory"
+          category = Category.find_or_create_by bgg_id: row["id"], name: row["value"]
+          gamescategory = GamesCategory.create(game: game, category: category) if category.valid?
+        elsif row["type"] == "boardgamemechanic"
+          mechanic = Mechanic.find_or_create_by bgg_id: row["id"], name: row["value"]
+          gamesmechanic = GamesMechanic.create(game: game, mechanic: mechanic) if mechanic.valid?
+        end
+      end
+    end
+  end
+end
